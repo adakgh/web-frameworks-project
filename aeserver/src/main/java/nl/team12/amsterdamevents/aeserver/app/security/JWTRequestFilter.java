5a853d16a5243fefe66411245dedd5c28cb434bb
J.L. Zheng <jieli.zheng@hva.nl>
2020-12-05 13:41:47 +0100

    4.2.2

--- aeserver/src/main/java/nl/team12/amsterdamevents/aeserver/app/security/JWTRequestFilter.java
+++ aeserver/src/main/java/nl/team12/amsterdamevents/aeserver/app/security/JWTRequestFilter.java
@@ -1,4 +1,102 @@
 package nl.team12.amsterdamevents.aeserver.app.security;
 
-public class JWTRequestFilter {
+import nl.team12.amsterdamevents.aeserver.app.exceptions.UnAuthorizedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.naming.AuthenticationException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
+
+@Component
+public class JWTRequestFilter extends OncePerRequestFilter {
+
+    // path prefixes that will be protected by the authentication filter
+    private static final Set<String> SECURED_PATHS =
+            Set.of("/aevents", "/registrations", "/users");
+
+    @Autowired
+    private JWToken jwToken;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain) throws IOException, ServletException {
+        JWToken jwToken = null;
+        String encryptedToken;
+
+        try {
+            // Get requested path
+            String servletPath = request.getServletPath();
+
+            // OPTIONS requests and non-secured area should pass through without check
+            if (HttpMethod.OPTIONS.matches(request.getMethod()) ||
+                    SECURED_PATHS.stream().noneMatch(servletPath::startsWith)) {
+                chain.doFilter(request, response);
+                return;
+            }
+
+            // get the encrypted token string from the authorization request header
+            encryptedToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+            // block the request if no token was found
+            if (encryptedToken != null) {
+                // remove the "Bearer" token prefix, if used
+                encryptedToken = encryptedToken.replace("Bearer", "");
+            }
+
+            // decode the token
+            // TODO: password????
+            jwToken = JWToken.decode(encryptedToken, this);
+
+            // Validate the token
+            if (jwToken == null) {
+                throw new UnAuthorizedException("You need to login first.");
+            }
+
+            // pass-on the token info as an attribute for the request
+            request.setAttribute(JWToken.JWT_ATTRIBUTE_NAME, jwToken);
+
+            // proceed with the chain
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            // aborting the chain
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authentication error");
+            return;
+        }
+
+
+////        JWToken jwToken = null;
+//        // get the encrypted token string from the authorization request header
+//        encryptedToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+//        // de encryptedToken is niet initialized
+//
+//        // block the request if no token was found
+//        if (encryptedToken != null) {
+//            // remove the "Bearer" token prefix, if used
+//            encryptedToken = encryptedToken.replace("Bearer", "");
+//        }
+//        // decode the token
+//        JwToken = JWToken.decode(encryptedToken, jwToken.passPhrase);
+//
+//        // Validate the token
+//        if (jwToken == null) {
+//            throw UnAuthorizedException("You need to login first.");
+//        }
+//
+//        // pass-on the token info as an attribute for the request
+//        request.setAttribute(JWToken.JWT_ATTRIBUTE_NAME,jwToken);
+//
+//        chain.doFilter(request, response);
+//    } catch (AuthenticationException exception){
+//        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authentication error");
+//        return;
+    }
 }
